8b7e9d7ebb9751c22929c30abcd89421ab706450
Sam-Davis07 <sam.r.davis07@gmail.com>
2026-08-13 00:37:20 +0530

    Solve Plus One leetcode problem no 66

---
+++ Arrays/plusOne.java
@@ -0,0 +1,26 @@
+public class plusOne {
+    public static void main(String[] args) {
+        int[] digits = { 9, 1, 8, 0, 9, 9 };
+        int[] result = plusOne(digits);
+
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
+
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+
+        return result;
+    }
+}
