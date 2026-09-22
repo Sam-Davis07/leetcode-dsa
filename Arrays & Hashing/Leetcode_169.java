479afa9750de29d01aa224885a03c463fdf5b58a
Sam-Davis07 <sam.r.davis07@gmail.com>
2026-09-22 08:32:36 +0530

    Solved Majority Elements

---
+++ Arrays & Hashing/Leetcode_169.java
@@ -0,0 +1,29 @@
+// 169. Majority Element
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+public class Leetcode_169 {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > nums.length / 2) {
+                return entry.getKey();
+            }
+        }
+
+        return 0;
+    }
+
+}
