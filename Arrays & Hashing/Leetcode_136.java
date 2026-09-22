cce20506cd8960a62761ae164e36da3efe904b95
Sam-Davis07 <sam.r.davis07@gmail.com>
2026-09-22 19:01:06 +0530

    Solved Single Number

---
+++ Arrays & Hashing/Leetcode_136.java
@@ -0,0 +1,33 @@
+// 136. Single Number
+
+// Example 1:
+
+// Input: nums = [2,2,1]
+
+// Output: 1
+
+// Example 2:
+
+// Input: nums = [4,1,2,1,2]
+
+// Output: 4
+
+import java.util.*;
+
+public class Leetcode_136 {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+
+        return 0;
+    }
+}
