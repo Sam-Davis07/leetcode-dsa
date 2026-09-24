d8c9c104ef94d74d34370ac5b9357bf5209f1973
Sam-Davis07 <sam.r.davis07@gmail.com>
2026-09-24 20:29:04 +0530

    Solved maximum subarray

---
+++ Arrays & Hashing/Leetcode_53.java
@@ -0,0 +1,29 @@
+// 53. Maximum Subarray
+
+// Example 1:
+
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+// Example 2:
+
+// Input: nums = [1]
+// Output: 1
+// Explanation: The subarray [1] has the largest sum 1.
+
+class Solution {
+
+    public int maxSubArray(int[] nums) {
+
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for(int i = 1 ; i < nums.length ; i++){
+            currentSum = Math.max(nums[i],currentSum+ nums[i]);
+            maxSum = Math.max(currentSum, maxSum);
+        }
+
+        return maxSum;
+
+    }
+}
